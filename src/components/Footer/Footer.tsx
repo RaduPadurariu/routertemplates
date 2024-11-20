b0d8daadf4ffccac6df121a37a260287eaa53412
radupadurariu <padurariuionutradu@gmail.com>
2024-11-20 14:26:36 +0200

    Implement responsive footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,28 @@
-import "./Footer.module.scss";
+import { social } from "../../data/portfolioData";
+import styles from "./Footer.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer_container}>
+        {" "}
+        <ul>
+          {social.map((el) => {
+            return (
+              <li key={el.id}>
+                <FontAwesomeIcon
+                  icon={el.icon}
+                  className={styles.social_icon}
+                />
+              </li>
+            );
+          })}
+        </ul>
+        <p>© 2024 Radu Padurariu & GorkCoder. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
