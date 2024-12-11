c6223cfb3bc40ff56f316a4246f298a5ce9469de
radupadurariu <padurariuionutradu@gmail.com>
2024-12-11 15:59:30 +0200

    Add Educational footer with quick links, recent posts and question section

--- src/data/educationalData.ts
+++ src/data/educationalData.ts
@@ -4,6 +4,11 @@ import {
   faInstagram,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
+import {
+  faMap,
+  faPaperPlane,
+  faPhoneAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const social = [
   { id: 1, icon: faFacebook },
@@ -22,4 +27,96 @@ export const educationalNavLinks = [
   { id: 7, text: "Contact", link: "/educational/contact" },
   { id: 8, text: "Personal", link: "/educational/myCourses" },
   { id: 9, text: "Templates", link: "/" },
+];
+
+export const blog = [
+  {
+    id: 1,
+    type: "admin",
+    date: "JAN. 18, 2021",
+    com: "3 COMMENTS ",
+    title: "Build your Dream Software & Engineering Career",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "/images/Templates/Educational/blog/b1.webp",
+  },
+  {
+    id: 2,
+    type: "admin",
+    date: "API. 25, 2022",
+    com: "5 COMMENTS ",
+    title: "Build your Dream Software & Engineering Career",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "/images/Templates/Educational/blog/b2.webp",
+  },
+  {
+    id: 3,
+    type: "user",
+    date: "MAY. 15, 2022",
+    com: "10 COMMENTS ",
+    title: "Build your Dream Software & Engineering Career",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "/images/Templates/Educational/blog/b3.webp",
+  },
+  {
+    id: 4,
+    type: "admin",
+    date: "JAN. 02, 2022",
+    com: "3 COMMENTS ",
+    title: "Build your Dream Software & Engineering Career",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "/images/Templates/Educational/blog/b4.webp",
+  },
+  {
+    id: 5,
+    type: "admin",
+    date: "DEC. 14, 2022",
+    com: "3 COMMENTS ",
+    title: "Build your Dream Software & Engineering Career",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "/images/Templates/Educational/blog/b5.webp",
+  },
+  {
+    id: 6,
+    type: "user",
+    date: "JAN. 18, 2021",
+    com: "12 COMMENTS ",
+    title: "Build your Dream Software & Engineering Career",
+    desc: "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    cover: "/images/Templates/Educational/blog/b6.webp",
+  },
+];
+
+// Footer
+
+export const footerSocial = [
+  { id: 1, icon: faFacebook },
+  { id: 2, icon: faTwitter },
+  { id: 3, icon: faInstagram },
+];
+
+export const footerExplore = [
+  { id: 1, link: "About Us" },
+  { id: 2, link: "Services" },
+  { id: 3, link: "Courses" },
+  { id: 4, link: "Blog" },
+  { id: 5, link: "Contact Us" },
+];
+
+export const footerQuickLinks = [
+  { id: 1, link: "Contact Us" },
+  { id: 2, link: "Pricing" },
+  { id: 3, link: "Terms & Conditions" },
+  { id: 4, link: "Privacy" },
+  { id: 5, link: "Feedbacks" },
+];
+
+export const footerQuestions = [
+  {
+    id: 1,
+    item: "203 Fake St. Mountain View, San Francisco, California, USA",
+    icon: faMap,
+  },
+  { id: 2, item: "+2 392 3929 210", icon: faPhoneAlt },
+
+  { id: 3, item: "info@yourdomain.com", icon: faPaperPlane },
 ];
--- src/templates/Educational/components/common/Footer/EducationalFooter.tsx
+++ src/templates/Educational/components/common/Footer/EducationalFooter.tsx
@@ -1,5 +1,165 @@
+import {
+  blog,
+  footerExplore,
+  footerQuestions,
+  footerQuickLinks,
+  footerSocial,
+} from "../../../../../data/educationalData";
+import styles from "./EducationalFooter.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendar,
+  faHeart,
+  faPaperPlane,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
 const EducationalFooter = () => {
-  return <div>EducationalFooter</div>;
+  return (
+    <>
+      {/* Newsletter */}
+      <section className={styles["educational-newsLetter"]}>
+        <div
+          className={`educational-container ${styles["educational-newsLetter__boxes"]}`}
+        >
+          <div className={styles["educational-newsLetter__boxes__left"]}>
+            <h1>Newsletter - Stay tune and get the latest update</h1>
+            <span>Far far away, behind the word mountains</span>
+          </div>
+          <div className={styles["educational-newsLetter__boxes__right"]}>
+            <input type="text" placeholder="Enter email address" />
+            <FontAwesomeIcon
+              icon={faPaperPlane}
+              className={styles["educational-newsLetter__icon"]}
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* Footer */}
+      <footer>
+        <div
+          className={`educational-container ${styles["educational-footer"]}`}
+        >
+          <div
+            className={`${styles["educational-footer__box"]} ${styles["educational-footer__logo"]}`}
+          >
+            <h1>ACADEMIA</h1>
+            <span>ONLINE EDUCATION & LEARNING</span>
+            <p>
+              A small river named Duden flows by their place and supplies it
+              with the necessary regelialia.
+            </p>
+
+            <ul className={styles["educational-footer__logo_iconList"]}>
+              {footerSocial.map((el) => {
+                return (
+                  <li
+                    key={el.id}
+                    className={
+                      styles["educational-footer__logo_iconList_icons"]
+                    }
+                  >
+                    <FontAwesomeIcon
+                      icon={el.icon}
+                      className={
+                        styles["educational-footer__logo_iconList_icons_icon"]
+                      }
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div
+            className={`${styles["educational-footer__box"]} ${styles["educational-footer__link"]}`}
+          >
+            <h3>Explore</h3>
+            <ul>
+              {footerExplore.map((el) => {
+                return (
+                  <li key={el.id}>
+                    <span>{el.link}</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div
+            className={`${styles["educational-footer__box"]} ${styles["educational-footer__link"]}`}
+          >
+            <h3>Quick Links</h3>
+            <ul>
+              {footerQuickLinks.map((el) => {
+                return (
+                  <li key={el.id}>
+                    <span>{el.link}</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className={styles["educational-footer__box"]}>
+            <h3>Recent Post</h3>
+            {blog.slice(0, 3).map((val) => (
+              <div className={styles["educational-footer__items"]}>
+                <div className="img">
+                  <img src={val.cover} alt="no-img" />
+                </div>
+                <div className="text">
+                  <span>
+                    <FontAwesomeIcon
+                      icon={faCalendar}
+                      className={styles["educational-footer__items__icon"]}
+                    />
+                    <label htmlFor="">{val.date}</label>
+                  </span>
+                  <span>
+                    <FontAwesomeIcon
+                      icon={faUser}
+                      className={styles["educational-footer__items__icon"]}
+                    />
+                    <label htmlFor="">{val.type}</label>
+                  </span>
+                  <h4>{val.title.slice(0, 40)}...</h4>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div
+            className={`${styles["educational-footer__box"]} ${styles["educational-footer__last"]}`}
+          >
+            <h3>Have a Questions?</h3>
+            <ul>
+              {footerQuestions.map((q) => {
+                return (
+                  <li key={q.id}>
+                    <FontAwesomeIcon
+                      icon={q.icon}
+                      className={styles["educational-footer__last__icon"]}
+                    />
+                    {q.item}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </footer>
+
+      {/* Legal rights */}
+      <div className={styles["educational-legal"]}>
+        <p>
+          Copyright ©2024 All rights reserved | This template is made with{" "}
+          <FontAwesomeIcon
+            icon={faHeart}
+            className={styles["educational-legal__icon"]}
+          />{" "}
+          by Radu Padurariu & GorkCoder
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default EducationalFooter;
