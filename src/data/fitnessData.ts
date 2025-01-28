485791e0274ee137330b71baa57a4862fe5a503c
radupadurariu <padurariuionutradu@gmail.com>
2025-01-28 15:06:02 +0200

    Add Feature page

--- src/data/fitnessData.ts
+++ src/data/fitnessData.ts
@@ -6,3 +6,10 @@ export const fitnessNavLinks = [
   { id: 5, text: "Personal", link: "fitness/myExercises" },
   { id: 6, text: "Contact", link: "/fitness/contact" },
 ];
+
+export const fitnessFeatures = [
+  { id: 1, title: "Weight Lifting", src: "/images/Templates/Fitness/1.svg" },
+  { id: 2, title: "Specific Muscle", src: "/images/Templates/Fitness/2.svg" },
+  { id: 3, title: "Flex Your Muscle", src: "/images/Templates/Fitness/3.svg" },
+  { id: 4, title: "Cardio Exercise", src: "/images/Templates/Fitness/4.svg" },
+];
