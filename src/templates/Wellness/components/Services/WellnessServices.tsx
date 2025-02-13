01bb367ce6f4ffb4f2f0427c340da696465e400c
radupadurariu <padurariuionutradu@gmail.com>
2025-02-13 17:55:11 +0200

    Add wellness services section

--- src/templates/Wellness/components/Services/WellnessServices.tsx
+++ src/templates/Wellness/components/Services/WellnessServices.tsx
@@ -1,7 +1,46 @@
+import { Link } from "react-router-dom";
+import { wellnessServices } from "../../../../data/wellnessData";
 import styles from "./WellnessServices.module.scss";
 
 const WellnessServices = () => {
-  return <div>WellnessServices</div>;
+  return (
+    <>
+      <section>
+        <div className={styles["wellness-services"]}>
+          <ul className={styles["wellness-services__items"]}>
+            {wellnessServices.map((service) => {
+              return (
+                <li
+                  key={service.id}
+                  className={styles["wellness-services__items_item"]}
+                >
+                  <div className={styles["wellness-services__items_item_info"]}>
+                    <h1>{service.title}</h1>
+                    <p>
+                      Mindfulness training isn’t limited to meditation, but it’s
+                      a great place to start. Meditation is both a skill and an
+                      experience that can positively impact your overall health
+                      and happiness.
+                    </p>
+                    <div
+                      className={
+                        styles["wellness-services__items_item_info_btn"]
+                      }
+                    >
+                      <Link to="#">Start your journey</Link>
+                    </div>
+                  </div>
+                  <div className={styles["wellness-services__items_item_img"]}>
+                    <img src={service.src} alt="" />
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default WellnessServices;
